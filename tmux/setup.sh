6eb8a1b6153ee4074d50d4d9f4a1dae825670b42
eduardosm7 <esm7@cin.ufpe.br>
2019-12-10 19:10:01 -0300

    [tmux] Add setup.sh and set shell

---
+++ tmux/setup.sh
@@ -0,0 +1,56 @@
+#/bin/bash
+
+set -e
+
+TPM_PATH="$HOME/.tmux/plugins/tpm"
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+function log {
+    echo "[$(date +'%H:%M:%S')] -> $1"
+}
+
+function install_tmux {
+    log "Checking if tmux is already installed..."
+
+    if type tmux > /dev/null; then
+        log "Tmux is already installed"
+    else
+        log "Tmux is not installed, installing now..."
+        sudo apt install --assume-yes tmux
+    fi
+}
+
+function install_dependencies {
+    log "Installing dependencies..."
+
+    # Tmux Plugin Manager
+    log "Checking if Tmux Plugin Manager is already installed..."
+    if [ -d $TPM_PATH ]; then
+        log "Tmux Plugin Manager is already installed"
+    else
+        log "Tmux Plugin Manager is not installed, installing now..."
+        git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+    fi
+}
+
+function create_symbolic_link {
+    log "Creating .tmux.conf symbolic link..."
+
+    TMUX_CONF_PATH=$HOME/.tmux.conf
+
+    if [ ! -L $TMUX_CONF_PATH ]; then
+        ln -s $DIR/.tmux.conf $TMUX_CONF_PATH
+    fi
+
+    log "Symbolic link created"
+}
+
+function main {
+    log "Setting Tmux up..."
+    install_tmux
+    install_dependencies
+    create_symbolic_link
+    log "Finished Tmux and dependencies installation with success"
+}
+
+main
